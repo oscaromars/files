88031d178a02e52097182a662c42ad98b2f286fe
analistadesarrollo06 <analistadesarrollo06@uteg.edu.ec>
2021-03-16 15:06:14 -0500

    test 1506

--- modules/financiero/views/pagosfacturas/subirpago.php
+++ modules/financiero/views/pagosfacturas/subirpago.php
@@ -95,21 +95,21 @@ $this->registerJs("
         <p class="text-danger"> <?= Yii::t("formulario", "Fields with * are required") ?> </p>
     </div>
     <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
-        <div class="col-md-7 col-sm-7 col-xs-7 col-lg-7">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div class="form-group">
                 <h4><span id="lbl_general"><?= Pagos::t("Pagos", "Student Data") ?></span></h4> 
             </div>
         </div>
         <div class='col-md-12 col-sm-12 col-xs-12 col-lg-12'>
-            <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                 <div class="form-group">
-                    <label for="txt_nombres" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_nombre1"><?= Yii::t("formulario", "Names") ?></label>
-                    <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
+                    <label for="txt_nombres" class="col-xs-12 col-sm-12 col-md-5 col-lg-5 control-label" id="lbl_nombre1"><?= Yii::t("formulario", "Names") ?></label>
+                    <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
                         <input type="text" class="form-control keyupmce" value="<?php echo $arr_persona['per_pri_nombre'] . " " . $arr_persona['per_seg_nombre'] . " " . $arr_persona['per_pri_apellido'] . " " . $arr_persona['per_seg_apellido'] ?>" id="txt_nombres" disabled data-type="alfa" placeholder="<?= Yii::t("formulario", "First Name") ?>">
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                 <div class="form-group">
                     <label for="txt_cedula" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_nombre1"><?= Pagos::t("Pagos", "Cell") ?></label>
                     <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
@@ -128,16 +128,16 @@ $this->registerJs("
         <div class='col-md-12 col-sm-12 col-xs-12 col-lg-12'>
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
                 <div class="form-group">
-                    <label for="cmb_ninteres" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label"><?= Especies::t("Academico", "Academic unit") ?></label>
-                    <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
+                    <label for="cmb_ninteres" class="col-xs-12 col-sm-12 col-md-5 col-lg-5 control-label"><?= Especies::t("Academico", "Academic unit") ?></label>
+                    <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
                         <?= Html::dropDownList("cmb_ninteres", $arr_persona['uaca_id'], array_merge([Yii::t("formulario", "Select")], $arr_unidad), ["class" => "form-control", "id" => "cmb_ninteres", "disabled" => "true"]) ?>
                     </div>
                 </div>  
             </div>
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" id="divModalidad">
                 <div class="form-group">
-                    <label for="cmb_modalidad" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label"><?= Especies::t("Academico", "Modality") ?></label>
-                    <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
+                    <label for="cmb_modalidad" class="col-xs-12 col-sm-12 col-md-5 col-lg-5 control-label"><?= Especies::t("Academico", "Modality") ?></label>
+                    <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
                         <?= Html::dropDownList("cmb_modalidad", $arr_persona['mod_id'], array_merge([Yii::t("formulario", "Select")], $arr_modalidad), ["class" => "form-control", "id" => "cmb_modalidad", "disabled" => "true"]) ?>
                     </div>
                 </div>
@@ -305,7 +305,7 @@ $this->registerJs("
                 </div>
             </div> 
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="pago_stripe"  style="display:none; justify-content: center;background-color: antiquewhite;flex-direction:column;border-radius:6px;justify-content:space-between">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="pago_stripe"  style="display:none; justify-content: center;background-color: lightblue;flex-direction:column;border-radius:6px;justify-content:space-between">
             <!------------------------------------------------------->
             <!----- INI PAGO STRIPE --------------------------------->                
             <!------------------------------------------------------->

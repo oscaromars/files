addc842225f9d29b42373d23114761648ddf08ec
GianVergara <analistadesarrollo02@uteg.edu.ec>
2021-03-16 13:54:34 -0500

    insert para cruce y detalle cruce

--- modules/financiero/models/Cruce.php
+++ modules/financiero/models/Cruce.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace app\modules\models\academico;
-
+namespace app\modules\financiero\models;
+use yii\data\ArrayDataProvider;
 use Yii;
+use app\models\Utilities;
 
 /**
  * This is the model class for table "cruce".
@@ -101,5 +102,181 @@ class Cruce extends \yii\db\ActiveRecord
     public function getDetalleCruces()
     {
         return $this->hasMany(DetalleCruce::className(), ['cru_id' => 'cru_id']);
+    }
+
+    /**
+     * Function insertarCruce
+     * @author  Giovanni Vergara <analistadesarrollo02@uteg.edu.ec>;
+     * @param
+     * @return cru_id
+     */
+    public function insertarCruce($est_id, $pfes_id, $cru_comprobante, $cru_fecha_comprobante, $cru_saldo_favor_inicial, $cru_saldo_favor, $cru_estado_cruce, $cru_usu_ingreso) {
+        $con = \Yii::$app->db_facturacion;
+        $trans = $con->getTransaction(); // se obtiene la transacción actual
+        if ($trans !== null) {
+            $trans = null; // si existe la transacción entonces no se crea una
+        } else {
+            $trans = $con->beginTransaction(); // si no existe la transacción entonces se crea una
+        }
+        $fecha = date(Yii::$app->params["dateTimeByDefault"]);
+        $param_sql = "cru_estado";
+        $bdet_sql = "1";
+
+        $param_sql .= ", cru_estado_logico";
+        $bdet_sql .= ", 1";
+
+        $param_sql .= ", cru_fecha_creacion";
+        $bdet_sql .= ", :cru_fecha_creacion";
+
+        if (isset($est_id)) {
+            $param_sql .= ", est_id";
+            $bdet_sql .= ", :est_id";
+        }
+        if (isset($pfes_id)) {
+            $param_sql .= ", pfes_id";
+            $bdet_sql .= ", :pfes_id";
+        }
+        if (isset($cru_comprobante)) {
+            $param_sql .= ", cru_comprobante";
+            $bdet_sql .= ", :cru_comprobante";
+        }
+        if (isset($cru_fecha_comprobante)) {
+            $param_sql .= ", cru_fecha_comprobante";
+            $bdet_sql .= ", :cru_fecha_comprobante";
+        }
+        if (isset($cru_saldo_favor_inicial)) {
+            $param_sql .= ", cru_saldo_favor_inicial";
+            $bdet_sql .= ", :cru_saldo_favor_inicial";
+        }
+        if (isset($cru_saldo_favor)) {
+            $param_sql .= ", cru_saldo_favor";
+            $bdet_sql .= ", :cru_saldo_favor";
+        }
+        if (isset($cru_estado_cruce)) {
+            $param_sql .= ", cru_estado_cruce";
+            $bdet_sql .= ", :cru_estado_cruce";
+        }
+        if (isset($cru_usu_ingreso)) {
+            $param_sql .= ", cru_usu_ingreso";
+            $bdet_sql .= ", :cru_usu_ingreso";
+        }  
+        try {
+            $sql = "INSERT INTO " . $con->dbname . ".cruce ($param_sql) VALUES($bdet_sql)";
+            $comando = $con->createCommand($sql);
+            if (isset($est_id)) {
+                $comando->bindParam(':est_id', $est_id, \PDO::PARAM_INT);
+            }
+            if (isset($pfes_id)) {
+                $comando->bindParam(':pfes_id', $pfes_id, \PDO::PARAM_INT);
+            }
+            if (isset($cru_comprobante)) {
+                $comando->bindParam(':cru_comprobante', $cru_comprobante, \PDO::PARAM_STR);
+            }
+            if (!empty((isset($cru_fecha_comprobante)))) {
+                $comando->bindParam(':cru_fecha_comprobante', $cru_fecha_comprobante, \PDO::PARAM_STR);
+            }
+            if (!empty((isset($cru_saldo_favor_inicial)))) {
+                $comando->bindParam(':cru_saldo_favor_inicial', $cru_saldo_favor_inicial, \PDO::PARAM_STR);
+            }
+            if (!empty((isset($cru_saldo_favor)))) {
+                $comando->bindParam(':cru_saldo_favor', $cru_saldo_favor, \PDO::PARAM_STR);
+            }           
+            if (!empty((isset($cru_estado_cruce)))) {
+                $comando->bindParam(':cru_estado_cruce', $cru_estado_cruce, \PDO::PARAM_STR);
+            }
+            if (!empty((isset($cru_usu_ingreso)))) {
+                $comando->bindParam(':cru_usu_ingreso', $cru_usu_ingreso, \PDO::PARAM_INT);
+            }
+            $comando->bindParam(":cru_fecha_creacion", $fecha, \PDO::PARAM_STR);
+            $result = $comando->execute();
+            if ($trans !== null)
+                $trans->commit();
+            return $con->getLastInsertID($con->dbname . '.cruce');
+        } catch (Exception $ex) {
+            if ($trans !== null)
+                $trans->rollback();
+            return $ex;
+        }
+    }
+
+     /**
+     * Function insertarDetallecruce
+     * @author  Giovanni Vergara <analistadesarrollo02@uteg.edu.ec>;
+     * @param
+     * @return dcru_id
+     */
+    public function insertarDetallecruce($cru_id, $dcru_comprobante_cruce, $dcru_fecha_comprobante_cruce, $dcru_valor_cruce, $dcru_observacion, $dcru_usu_ingreso) {
+        $con = \Yii::$app->db_facturacion;
+        $trans = $con->getTransaction(); // se obtiene la transacción actual
+        if ($trans !== null) {
+            $trans = null; // si existe la transacción entonces no se crea una
+        } else {
+            $trans = $con->beginTransaction(); // si no existe la transacción entonces se crea una
+        }
+        $fecha = date(Yii::$app->params["dateTimeByDefault"]);
+        $param_sql = "dcru_estado";
+        $bdet_sql = "1";
+
+        $param_sql .= ", dcru_estado_logico";
+        $bdet_sql .= ", 1";
+
+        $param_sql .= ", dcru_fecha_creacion";
+        $bdet_sql .= ", :dcru_fecha_creacion";
+
+        if (isset($cru_id)) {
+            $param_sql .= ", cru_id";
+            $bdet_sql .= ", :cru_id";
+        }
+        if (isset($dcru_comprobante_cruce)) {
+            $param_sql .= ", dcru_comprobante_cruce";
+            $bdet_sql .= ", :dcru_comprobante_cruce";
+        }
+        if (isset($dcru_fecha_comprobante_cruce)) {
+            $param_sql .= ", dcru_fecha_comprobante_cruce";
+            $bdet_sql .= ", :dcru_fecha_comprobante_cruce";
+        }
+        if (isset($dcru_valor_cruce)) {
+            $param_sql .= ", dcru_valor_cruce";
+            $bdet_sql .= ", :dcru_valor_cruce";
+        }
+        if (isset($dcru_observacion)) {
+            $param_sql .= ", dcru_observacion";
+            $bdet_sql .= ", :dcru_observacion";
+        }
+        if (isset($dcru_usu_ingreso)) {
+            $param_sql .= ", dcru_usu_ingreso";
+            $bdet_sql .= ", :dcru_usu_ingreso";
+        }        
+        try {
+            $sql = "INSERT INTO " . $con->dbname . ".detalle_cruce ($param_sql) VALUES($bdet_sql)";
+            $comando = $con->createCommand($sql);
+            if (isset($cru_id)) {
+                $comando->bindParam(':cru_id', $cru_id, \PDO::PARAM_INT);
+            }
+            if (isset($dcru_comprobante_cruce)) {
+                $comando->bindParam(':dcru_comprobante_cruce', $dcru_comprobante_cruce, \PDO::PARAM_STR);
+            }
+            if (isset($dcru_fecha_comprobante_cruce)) {
+                $comando->bindParam(':dcru_fecha_comprobante_cruce', $dcru_fecha_comprobante_cruce, \PDO::PARAM_STR);
+            }
+            if (!empty((isset($dcru_valor_cruce)))) {
+                $comando->bindParam(':dcru_valor_cruce', $dcru_valor_cruce, \PDO::PARAM_STR);
+            }
+            if (!empty((isset($dcru_observacion)))) {
+                $comando->bindParam(':dcru_observacion', ucfirst(mb_strtolower($dcru_observacion, 'UTF-8')), \PDO::PARAM_STR);
+            }
+            if (!empty((isset($dcru_usu_ingreso)))) {
+                $comando->bindParam(':dcru_usu_ingreso', $dcru_usu_ingreso, \PDO::PARAM_INT);
+            }
+            $comando->bindParam(":dcru_fecha_creacion", $fecha, \PDO::PARAM_STR);
+            $result = $comando->execute();
+            if ($trans !== null)
+                $trans->commit();
+            return $con->getLastInsertID($con->dbname . '.detalle_cruce');
+        } catch (Exception $ex) {
+            if ($trans !== null)
+                $trans->rollback();
+            return $ex;
+        }
     }
 }

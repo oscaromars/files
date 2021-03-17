5f940800318f4698a9c64a2f565a55d5f412a915
analistadesarrollo06 <analistadesarrollo06@uteg.edu.ec>
2021-03-17 17:17:10 -0500

    miercoles 1716

--- modules/financiero/models/Cruce.php
+++ modules/financiero/models/Cruce.php
@@ -281,8 +281,8 @@ class Cruce extends \yii\db\ActiveRecord
     }
 
     /**
-     * Function insertarCruce
-     * @author  Giovanni Vergara <analistadesarrollo02@uteg.edu.ec>;
+     * Function consultarSaldos
+     * @author  Galo Aguirre <analistadesarrollo06@uteg.edu.ec>;
      * @param
      * @return cru_id
      */
@@ -301,7 +301,9 @@ class Cruce extends \yii\db\ActiveRecord
                 $str_search .= "inte.cru_fecha_creacion <= :fec_fin AND ";
             }
         }
-        $sql = "SELECT cru.cru_fecha_creacion,
+        $sql = "SELECT cru.cru_id,
+                       per.per_id,
+                       cru.cru_fecha_creacion,
                        concat(ifnull(per.per_pri_nombre,''),' ',ifnull(per.per_seg_nombre,'')) as nombres,
                        concat(ifnull(per.per_pri_apellido,''),' ',ifnull(per.per_seg_apellido,'')) as apellidos, 
                        cru.cru_saldo_favor_inicial,
@@ -312,6 +314,77 @@ class Cruce extends \yii\db\ActiveRecord
                  WHERE cru_estado = :cru_estado";
         
         $comando = $con->createCommand($sql);
+        $comando->bindParam(":cru_estado", $estado, \PDO::PARAM_STR);
+
+        if (isset($arrFiltro) && count($arrFiltro) > 0) {
+            $search_cond = "%" . $arrFiltro["search"] . "%";
+            $fecha_ini = $arrFiltro["f_ini"] . " 00:00:00";
+            $fecha_fin = $arrFiltro["f_fin"] . " 23:59:59";
+
+            if ($arrFiltro['f_ini'] != "" && $arrFiltro['f_fin'] != "") {
+                $comando->bindParam(":fec_ini", $fecha_ini, \PDO::PARAM_STR);
+                $comando->bindParam(":fec_fin", $fecha_fin, \PDO::PARAM_STR);
+            }
+            $empresa = $arrFiltro["company"];
+            $comando->bindParam(":search", $search_cond, \PDO::PARAM_STR);
+            if ($arrFiltro['company'] != "" && $arrFiltro['company'] > 0) {
+                $comando->bindParam(":emp_id", $empresa, \PDO::PARAM_STR);
+            }
+        }
+        $resultData = $comando->queryAll();
+        $dataProvider = new ArrayDataProvider([
+            'key' => 'id',
+            'allModels' => $resultData,
+            'pagination' => [
+                'pageSize' => Yii::$app->params["pageSize"],
+            ],
+            'sort' => [
+                'attributes' => [
+                    'DNI',
+                    'nombres',
+                    'apellidos',
+                    'empresa',
+                ],
+            ],
+        ]);
+
+        if ($onlyData) {
+            return $resultData;
+        } else {
+            return $dataProvider;
+        }
+    }//function consultarSaldos
+
+    /**
+     * Function consultarCruces
+     * @author  Galo Aguirre <analistadesarrollo06@uteg.edu.ec>;
+     * @param
+     * @return cru_id
+     */
+    public function consultarCruces($dcru_id, $arrFiltro = array(), $onlyData = false){
+        $con    = \Yii::$app->db_facturacion;
+        $con1   = \Yii::$app->db_asgard;
+        $con2   = \Yii::$app->db_academico;
+
+        $estado = 1;
+        if (isset($arrFiltro) && count($arrFiltro) > 0) {
+            $str_search = "(per.per_pri_nombre like :search OR ";
+            $str_search .= "per.per_seg_nombre like :search OR ";
+            $str_search .= "per.per_pri_apellido like :search OR ";
+            if ($arrFiltro['f_ini'] != "" && $arrFiltro['f_fin'] != "") {
+                $str_search .= "inte.cru_fecha_creacion >= :fec_ini AND ";
+                $str_search .= "inte.cru_fecha_creacion <= :fec_fin AND ";
+            }
+        }
+        $sql = "SELECT *
+                  FROM ". $con->dbname  .".detalle_cruce dcru
+                 WHERE dcru.dcru_id            = :dcru_id
+                   AND dcru.dcru_estado        = :cru_estado
+                   AND dcru.dcru_estado_logico = :cru_estado";
+
+        
+        $comando = $con->createCommand($sql);
+        $comando->bindParam(":dcru_id", $dcru_id, \PDO::PARAM_STR);
         $comando->bindParam(":cru_estado", $estado, \PDO::PARAM_STR);
 
         if (isset($arrFiltro) && count($arrFiltro) > 0) {

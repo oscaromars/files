99c8c0c5241be3d44a1c56deed7198bd6dbe95dd
GianVergara <analistadesarrollo02@uteg.edu.ec>
2021-03-24 15:46:30 -0500

    actualizar tabla y modelo de namespace app\modules\financiero\models;

--- modules/financiero/models/PagosFacturaEstudiante.php
+++ modules/financiero/models/PagosFacturaEstudiante.php
@@ -11,51 +11,57 @@ use app\models\Utilities;
  *
  * @property int $pfes_id
  * @property int $est_id
- * @property string $pfes_referencia
+ * @property string $pfes_concepto
+ * @property int|null $ban_id
+ * @property string|null $pfes_referencia
  * @property int $fpag_id
- * @property double $pfes_valor_pago
- * @property string $pfes_fecha_pago
- * @property string $pfes_observacion
+ * @property float $pfes_valor_pago
+ * @property string|null $pfes_fecha_pago
+ * @property string|null $pfes_observacion
  * @property string $pfes_archivo_pago
- * @property string $pfes_fecha_registro
+ * @property string|null $pfes_fecha_registro
  * @property int $pfes_usu_ingreso
  * @property string $pfes_estado
  * @property string $pfes_fecha_creacion
- * @property string $pfes_fecha_modificacion
+ * @property string|null $pfes_fecha_modificacion
  * @property string $pfes_estado_logico
  *
+ * @property Cruce[] $cruces
  * @property FormaPago $fpag
  */
-class PagosFacturaEstudiante extends \yii\db\ActiveRecord {
-
+class PagosFacturaEstudiante extends \yii\db\ActiveRecord
+{
     /**
      * {@inheritdoc}
      */
-    public static function tableName() {
+    public static function tableName()
+    {
         return 'pagos_factura_estudiante';
     }
 
     /**
      * @return \yii\db\Connection the database connection used by this AR class.
      */
-    public static function getDb() {
+    public static function getDb()
+    {
         return Yii::$app->get('db_facturacion');
     }
 
     /**
      * {@inheritdoc}
      */
-    public function rules() {
+    public function rules()
+    {
         return [
-            [['pfes_id', 'est_id', 'fpag_id', 'pfes_valor_pago', 'pfes_archivo_pago', 'pfes_usu_ingreso', 'pfes_estado', 'pfes_estado_logico'], 'required'],
-            [['pfes_id', 'est_id', 'fpag_id', 'pfes_usu_ingreso'], 'integer'],
+            [['est_id', 'pfes_concepto', 'fpag_id', 'pfes_valor_pago', 'pfes_archivo_pago', 'pfes_usu_ingreso', 'pfes_estado', 'pfes_estado_logico'], 'required'],
+            [['est_id', 'ban_id', 'fpag_id', 'pfes_usu_ingreso'], 'integer'],
             [['pfes_valor_pago'], 'number'],
             [['pfes_fecha_pago', 'pfes_fecha_registro', 'pfes_fecha_creacion', 'pfes_fecha_modificacion'], 'safe'],
+            [['pfes_concepto'], 'string', 'max' => 3],
             [['pfes_referencia'], 'string', 'max' => 50],
             [['pfes_observacion'], 'string', 'max' => 500],
             [['pfes_archivo_pago'], 'string', 'max' => 200],
             [['pfes_estado', 'pfes_estado_logico'], 'string', 'max' => 1],
-            [['pfes_id'], 'unique'],
             [['fpag_id'], 'exist', 'skipOnError' => true, 'targetClass' => FormaPago::className(), 'targetAttribute' => ['fpag_id' => 'fpag_id']],
         ];
     }
@@ -63,10 +69,13 @@ class PagosFacturaEstudiante extends \yii\db\ActiveRecord {
     /**
      * {@inheritdoc}
      */
-    public function attributeLabels() {
+    public function attributeLabels()
+    {
         return [
             'pfes_id' => 'Pfes ID',
             'est_id' => 'Est ID',
+            'pfes_concepto' => 'Pfes Concepto',
+            'ban_id' => 'Ban ID',
             'pfes_referencia' => 'Pfes Referencia',
             'fpag_id' => 'Fpag ID',
             'pfes_valor_pago' => 'Pfes Valor Pago',
@@ -83,9 +92,22 @@ class PagosFacturaEstudiante extends \yii\db\ActiveRecord {
     }
 
     /**
+     * Gets query for [[Cruces]].
+     *
      * @return \yii\db\ActiveQuery
      */
-    public function getFpag() {
+    public function getCruces()
+    {
+        return $this->hasMany(Cruce::className(), ['pfes_id' => 'pfes_id']);
+    }
+
+    /**
+     * Gets query for [[Fpag]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFpag()
+    {
         return $this->hasOne(FormaPago::className(), ['fpag_id' => 'fpag_id']);
     }
 

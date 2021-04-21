ded30a889fa074d8011bacdb4202e7071ddf3fdb
GianVergara <analistadesarrollo02@uteg.edu.ec>
2021-04-21 16:54:06 -0500

    Vista para crear nuevas cursos integracion educativa con asgard

--- modules/academico/controllers/UsuarioeducativaController.php
+++ modules/academico/controllers/UsuarioeducativaController.php
@@ -48,8 +48,7 @@ class UsuarioeducativaController extends \app\components\CController {
                 'arr_periodoAcademico' => ArrayHelper::map(array_merge([["id" => "0", "name" => Yii::t("formulario", "All")]], $arr_periodoAcademico), "id", "name"),
                 'arr_asignatura' => ArrayHelper::map(array_merge([["id" => "0", "name" => Yii::t("formulario", "All")]], $arr_asignatura), "id", "name"),
                 'model' => $model,
-            ]);
-        //}
+            ]);       
     }
 
     public function actionCargarusuario() {
@@ -417,5 +416,65 @@ class UsuarioeducativaController extends \app\components\CController {
                 ])
         );
         $report->mpdf->Output('Reporte_' . date("Ymdhis") . ".pdf", ExportFile::OUTPUT_TO_DOWNLOAD);
+    }
+
+    public function actionNew() { 
+        $mod_periodo = new PeriodoAcademicoMetIngreso();
+        $mod_asignatura = new Asignatura(); 
+        $mod_educativa = new CursoEducativa();
+        $data = Yii::$app->request->get();
+        if (Yii::$app->request->isAjax) {
+            $data = Yii::$app->request->post();            
+        }   
+            $arr_asignatura = $mod_asignatura->consultarAsignaturasxuacaid(1);
+            $arr_periodoAcademico = $mod_periodo->consultarPeriodoAcademicotodos();
+            return $this->render('new', [  
+                'arr_periodoAcademico' => ArrayHelper::map(array_merge([["id" => "0", "name" => Yii::t("formulario", "Select")]], $arr_periodoAcademico), "id", "name"),
+                'arr_asignatura' => ArrayHelper::map(array_merge([["id" => "0", "name" => Yii::t("formulario", "Select")]], $arr_asignatura), "id", "name"),
+                'model' => $model,
+            ]);       
+    }
+    public function actionSavecurso() {
+        $per_id = @Yii::$app->session->get("PB_perid");
+        $usuario = @Yii::$app->user->identity->usu_id;
+        if (Yii::$app->request->isAjax) {
+            $data = Yii::$app->request->post();
+            $periodo = $data["periodo"];
+            $materia = $data["materia"];
+            $codigoaula = $data["codigoaula"];
+            $nombreaula = $data["nombreaula"];
+            $con = \Yii::$app->db_academico;
+            $transaction = $con->beginTransaction();
+            try {
+                //$mod_educativa = new CursoEducativa();
+                //$savecurso = $mod_educativa->insertarCursoeducativa($periodo, $materia, $codigoaula, $nombreaula,  $usuario);
+                //if ($savecurso) {
+                    $exito = 1;
+                //}
+                if ($exito) {
+                    $transaction->commit();
+                    $message = array(
+                        "wtmessage" => Yii::t("notificaciones", "Se ha guardado el curso."),
+                        "title" => Yii::t('jslang', 'Success'),
+                    );
+                    return Utilities::ajaxResponse('OK', 'alert', Yii::t("jslang", "Sucess"), false, $message);
+                } else {
+                    $transaction->rollback();
+                    $message = array(
+                        "wtmessage" => Yii::t("notificaciones", "Error al grabar." . $mensaje),
+                        "title" => Yii::t('jslang', 'Error'),
+                    );
+                    return Utilities::ajaxResponse('NO_OK', 'alert', Yii::t("jslang", "Error"), false, $message);
+                }
+            } catch (Exception $ex) {
+                $transaction->rollback();
+                $message = array(
+                    "wtmessage" => Yii::t("notificaciones", "Error al grabar."),
+                    "title" => Yii::t('jslang', 'Error'),
+                );
+                return Utilities::ajaxResponse('NO_OK', 'alert', Yii::t("jslang", "Error"), false, $message);
+            }
+            return;
+        }
     }
 }  
--- modules/academico/views/usuarioeducativa/js/usuarioeducativa.js
+++ modules/academico/views/usuarioeducativa/js/usuarioeducativa.js
@@ -17,6 +17,9 @@ $(document).ready(function() {
         actualizarGridCureducativa();
     });
     
+    $('#btn_newcurso').click(function () {
+        savecurso();
+    });
 
     $('#cmb_unidad_dises').change(function () {
         var link = $('#txth_base').val() + "/academico/distributivo/listarestudiantespago";
@@ -168,4 +171,31 @@ function exportPdfcurso() {
     var periodo =  $('#cmb_periodo option:selected').val();
     var asignatura = $('#cmb_asignatura option:selected').val(); 
     window.location.href = $('#txth_base').val() + "/academico/usuarioeducativa/exppdfestcurso?pdf=1&search=" + search + "&periodo=" + periodo + "&asignatura=" + asignatura;
+}
+
+function savecurso() {
+    var link = $('#txth_base').val() + "/academico/usuarioeducativa/savecurso";
+    var arrParams = new Object();
+    arrParams.periodo = $('#cmb_periodonew option:selected').val();
+    arrParams.materia = $('#cmb_asignaturanew option:selected').val();
+    arrParams.codigoaula = $('#txt_codigonew').val();
+    arrParams.nombreaula = $('#txt_aulanew').val();
+    if ($('#cmb_periodonew option:selected').val() != 0) {           
+     if ($('#cmb_asignaturanew option:selected').val() != 0) { 
+        if (!validateForm()) {
+            requestHttpAjax(link, arrParams, function(response) {
+                showAlert(response.status, response.label, response.message);
+                if (response.status == "OK") {
+                    setTimeout(function() {
+                        window.location.href = $('#txth_base').val() + "/academico/usuarioeducativa/index";
+                    }, 3000);
+                }
+            }, true);
+        }
+    } else {
+        showAlert('NO_OK', 'error', {"wtmessage": 'Nombre Aula: El campo no debe estar vacío.', "title": 'Error'});
+      } 
+    } else {
+        showAlert('NO_OK', 'error', {"wtmessage": 'Código Aula: El campo no debe estar vacío.', "title": 'Error'});
+     }  
 }

ccdfb3af1e9c3775c0e43d287fb51b2ad3e24f62
root <root@localhost.localdomain>
2021-06-04 15:33:15 -0500

    Acta de Calificaciones adaptada --Jorge Paladines

--- modules/academico/views/calificacionregistrodocente/acta_body.php
+++ modules/academico/views/calificacionregistrodocente/acta_body.php
@@ -3,6 +3,8 @@
 	//$lblTotalCost = "TOTAL COSTO";
 	if($costoProgCarrera_est != $costoCarrera_est)
 		$lblTotalCost .= " PERIODO" . strtoupper($periodoAcad);
+
+	// \app\models\Utilities::putMessageLogFile($model);
  ?>
 <style>
     .bold{
@@ -53,7 +55,7 @@
 <div>
     <div style="text-align: center">
         <br><br>
-        <p><span class="bold">MIAMI BUSINESS TECHNOLOGICAL UNIVERSITY</span></p>
+        <p><span class="bold">UNIVERSIDAD TECNOLÓGICA EMPRESARIAL DE GUAYAQUIL</span></p>
         <br><br><br>
     </div>
      <div style="text-align: center">
@@ -68,17 +70,15 @@
 		  <tr class="tr">
 		  	<td colspan="5" class="td"><span class="bold">FACULTAD:</span><br>  </td>
 		  </tr>
-		
 		  <tr class="tr">
-					<td colspan="1" class="aleft td"><span class="bold">COURSE</span></td>
-					<td colspan="1" class="aleft td"><span class="bold"><?= $model[0]['asi_nombre'] ?></span></td>
-					<td colspan="1" class="aleft td"><span class="bold">NUMBERS</span></td>
-					<td colspan="1" class="aleft td"><span class="bold"><?= $model[0]['par_nombre'] ?></span></td>
+			<td colspan="2" class="aleft td"><span class="bold">CURSO:</span></td>
+		    <td colspan="3" class="aleft td">    <?= $model[0]['asi_nombre'] ?>   </td>
 		  </tr>
 		  <tr class="tr">
 		    <td colspan="2" class="aleft td"><span class="bold">PERIODO:</span></td>
-		    <td colspan="2" class="aleft td">    <?= $model[0]['paca_nombre'] ?>   </td>
+		    <td colspan="3" class="aleft td">    <?= $model[0]['paca_nombre'] ?>   </td>
 		  </tr>
+
 		</table>
     	<br>
     	<br>
@@ -86,16 +86,15 @@
         	 <thead>
         		<tr class="tr">
 					<td  class="td"><span class="bold">NÚMERO</span></td>
-					<td  class="td"><span class="bold">STUDENTS</span></td>
-					<td  class="td"><span class="bold">MDTERM</span></td>
-					<td  class="td"><span class="bold">FINAL</span></td>
-					<td  class="td"><span class="bold">COURSE SCORE</span></td>
+					<td  class="td"><span class="bold">ESTUDIANTE</span></td>
+					<td  class="td"><span class="bold">PARCIAL 1</span></td>
+					<td  class="td"><span class="bold">PARCIAL 2</span></td>
+					<td  class="td"><span class="bold">SUPLETORIO</span></td>
+					<td  class="td"><span class="bold">PUNTAJE FINAL</span></td>
                 </tr>
 			   
 		  </thead>
 			<tbody>
-				
-				
 
                 <?php 
                 $i=0;
@@ -103,11 +102,12 @@
                 	$i++;
                           
             	echo "<tr style='border: 1px solid #002060;'>
-            			<td style='text-align: center' >".$i."</td>
-            			<td style='text-align: center'>".$value['Nombres_completos']."</td>
-            			<td style='text-align: center'>".$value['Parcial_I']."</td>
-            			<td style='text-align: center'>".  $value['Parcial_II'] ."</td>
-            			<td style='text-align: center'>".$value['promedio_final']."</td> 
+            			<td style='text-align: center'>". $i."</td>
+            			<td style='text-align: center'>".  $value['Nombres_completos']."</td>
+            			<td style='text-align: center'>".  $value['parcial_1']."</td>
+            			<td style='text-align: center'>".  $value['parcial_2'] ."</td>
+            			<td style='text-align: center'>".  $value['supletorio'] ."</td>
+            			<td style='text-align: center'>".  $value['promedio_final']."</td> 
             			</tr>";
                            
                   } ?>

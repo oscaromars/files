8401695d833b3a88d59114162e73e8e222aeae7d
root <root@localhost.localdomain>
2021-06-07 18:45:38 -0500

    hoja de inscripcion asgard varios Julio Lopez

--- modules/academico/views/tpl_registropagomatricula/registro.php
+++ modules/academico/views/tpl_registropagomatricula/registro.php
@@ -164,14 +164,14 @@
                 <td> ASOCIACION DE ESTUDIANTES</td>
                 <td> </td>
                 <td> </td>
-                <td> </td>' . $detallePagosRon[0]['ron_valor_aso_estudiante']
+                <td> </td>' . $detallePagosRon[0]['ron_valor_aso_estudiante'] .
              '</tr>';
          echo '<tr> 
                 <td> </td>
                 <td> VARIODS ADMINISTRATIVOS</td>
                 <td> </td>
                 <td> </td>
-                <td> </td>' . $detallePagosRon[0]['ron_valor_gastos_adm']
+                <td> </td>' . $detallePagosRon[0]['ron_valor_gastos_adm'] .
              '</tr>';
         
         

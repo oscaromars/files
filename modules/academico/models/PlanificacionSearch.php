6802262b341fe2a445944faa189d14c64a92e540
lisbethgonzalez <lisbethgon.21@gmail.com>
2021-06-15 15:06:32 -0500

    por merger cambios en archivo DistributivoAcademicoSearch.php de asgard - Lisbeth Gonzalez

--- modules/academico/models/PlanificacionSearch.php
+++ modules/academico/models/PlanificacionSearch.php
@@ -65,7 +65,7 @@ class PlanificacionSearch extends Planificacion {
         $con_db = \Yii::$app->db;
 
             $sql = "select est.est_id , 
-                ifnull(CONCAT(per.per_pri_apellido, ' ', per.per_seg_apellido, ' ', per.per_pri_nombre), '') as estudiante,
+                ifnull(CONCAT(ifnull(per.per_pri_apellido,''), ' ', ifnull(per.per_seg_apellido,''), ' ', ifnull(per.per_pri_nombre,'')), '') as estudiante,
                 per.per_cedula as cedula, 
                 CONCAT(baca.baca_nombre, ' ', sa.saca_nombre, ' ', sa.saca_anio) as semestre,
                 mod_descripcion as modalidad,

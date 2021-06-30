51ed2bcd7711fc5d9f12c94e35c1600f657a448b
GianVergara <analistadesarrollo02@uteg.edu.ec>
2021-06-30 14:52:20 -0500

    depurar codigo

--- modules/academico/models/DistributivoCabeceraSearch.php
+++ modules/academico/models/DistributivoCabeceraSearch.php
@@ -6,7 +6,7 @@ use yii\base\Model;
 use Yii;
 
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -20,10 +20,10 @@ class DistributivoCabeceraSearch extends DistributivoCabecera
     {
         return [
          [['pro_id'], 'integer'],
-        
+
         ];
     }
-    
+
     /**
      * {@inheritdoc}
      */
@@ -32,26 +32,26 @@ class DistributivoCabeceraSearch extends DistributivoCabecera
         // bypass scenarios() implementation in the parent class
         return Model::scenarios();
     }
-    
-    
+
+
      public function prueba(){
-         
+
          return "prueba";
      }
-    
-    
-    
+
+
+
     public function search($params)
     {
         $query = DistributivoCabecera::find();
-       
+
         // add conditions that should always apply here
 
         $dataProvider = new ActiveDataProvider(['query' => $query,   ]);
         $query->andFilterWhere([ 'dcab_estado_revision' => 1, ]);
         $this->load($params);
-        
-         
+
+
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
         // $query->where('0=1');
@@ -62,8 +62,8 @@ class DistributivoCabeceraSearch extends DistributivoCabecera
             'paca_id' => $this->paca_id,
         ]);
 
-        
-        
+
+
 //        $query->andFilterWhere(['like', 'daho_descripcion', $this->daho_descripcion])
 //            ->andFilterWhere(['like', 'daho_jornada', $this->daho_jornada])
 //                ->andFilterWhere(['like', 'daho_jornada', $this->daho_jornada]);

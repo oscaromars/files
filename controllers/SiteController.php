88fe370515b330347a806f281f090ac2ba1e9835
GianVergara <analistadesarrollo02@uteg.edu.ec>
2021-07-05 17:21:57 -0500

    cambios varios pagina de integracion ver dash

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -386,7 +386,7 @@ class SiteController extends CController {
             if($modEper != 0){
                 $url = Url::base(true) . "/site/dash";
             }
-            $url = Url::base(true) ."";
+            $url = Url::base(true) .""; // Se comenta y muestra la pagina de integracion de servicios
             return $this->goBack($url);
         } else {
             if ($model->getErrorSession())

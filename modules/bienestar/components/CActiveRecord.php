7c34aaa58e710b72bcce2ccb8576bd9203760cd9
GianVergara <analistadesarrollo02@uteg.edu.ec>
2021-07-19 17:00:44 -0500

    cambios varios

--- modules/bienestar/components/CActiveRecord.php
+++ modules/bienestar/components/CActiveRecord.php
@@ -12,15 +12,15 @@ use yii\helpers\Inflector;
 class CActiveRecord extends \yii\db\ActiveRecord {
     public static function getDb()
     {
-        $connection = new \app\components\CConnection(Yii::$app->controller->module->db_bienestar_estudiantil);
+        $connection = new \app\components\CConnection(Yii::$app->controller->module->db_bienestar);
         return $connection;
-        //return Yii::createObject(Yii::$app->controller->module->db_bienestar_estudiantil);
+        //return Yii::createObject(Yii::$app->controller->module->db_bienestar);
     }
 
     public static function tableName()
     {
-        if(Yii::$app->controller->module->db_bienestar_estudiantil["dbname"]){
-            $dbname = StringHelper::basename(Yii::$app->controller->module->db_bienestar_estudiantil["dbname"]);
+        if(Yii::$app->controller->module->db_bienestar["dbname"]){
+            $dbname = StringHelper::basename(Yii::$app->controller->module->db_bienestar["dbname"]);
             return '{{%' . $dbname . '.' . StringHelper::basename(get_called_class()) . '}}';
         }
         return '{{%' . Inflector::camel2id(StringHelper::basename(get_called_class()), '_') . '}}';

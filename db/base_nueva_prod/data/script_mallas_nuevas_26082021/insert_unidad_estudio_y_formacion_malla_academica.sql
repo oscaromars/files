19fef291841be7537d4aadcbb6a1bbf71c918c9c
GianVergara <analistadesarrollo02@uteg.edu.ec>
2021-08-27 11:55:05 -0500

    insert nuevas, asignaturas, malla academica, unidad estudio, formacion malla academica

---
+++ db/base_nueva_prod/data/script_mallas_nuevas_26082021/insert_unidad_estudio_y_formacion_malla_academica.sql
@@ -0,0 +1,14 @@
+-- unidad_estudio
+INSERT INTO `db_academico`.`unidad_estudio` (`uest_id`, `uest_nombre`, `uest_descripcion`, `uest_usuario_ingreso`, `uest_estado`, `uest_fecha_creacion`, `uest_estado_logico`)
+VALUES ('5', 'Internado Rotativo', 'Internado Rotativo', '1', '1', '2021-08-26 00:00:00', '1');
+
+INSERT INTO `db_academico`.`unidad_estudio` (`uest_id`, `uest_nombre`, `uest_descripcion`, `uest_usuario_ingreso`, `uest_estado`, `uest_fecha_creacion`, `uest_estado_logico`)
+VALUES ('6', 'Integración Curricular', 'Integración Curricular', '1', '1', '2021-08-26 00:00:00', '1');
+
+-- formacion_malla_academica
+INSERT INTO `db_academico`.`formacion_malla_academica` (`fmac_id`, `fmac_codigo`, `fmac_nombre`, `fmac_descripcion`, `fmac_usuario_ingreso`, `fmac_estado`, `fmac_fecha_creacion`, `fmac_estado_logico`)
+VALUES ('7', 'IR', 'Internado Rotativo', 'Internado Rotativo', '1', '1', '2021-08-26 00:00:00', '1');
+
+INSERT INTO `db_academico`.`formacion_malla_academica` (`fmac_id`, `fmac_codigo`, `fmac_nombre`, `fmac_descripcion`, `fmac_usuario_ingreso`, `fmac_estado`, `fmac_fecha_creacion`, `fmac_estado_logico`)
+VALUES ('8', 'PA', 'Praxis Ambiental', 'Praxis Ambiental', '1', '1', '2021-08-26 00:00:00', '1');
+

7606007d47e234a16978d8fa707b51dc205460d8
GianVergara <analistadesarrollo02@uteg.edu.ec>
2021-08-27 15:06:06 -0500

    insert nuevas, malla academica detalle y malla academica requisito

---
+++ db/base_nueva_prod/data/script_mallas_nuevas_26082021/3_insert_mallaacademica_nuevas.sql
@@ -0,0 +1,6 @@
+/* Cabecera malla academica */
+
+INSERT INTO `db_academico`.`malla_academica` (`maca_id`, `maca_tipo`, `maca_codigo`, `maca_nombre`, `maca_usuario_ingreso`, `maca_estado`, `maca_fecha_creacion`, `maca_estado_logico`) VALUES ('93', '2', 'GRA-0093', 'Nutrición y Dietética', '1', '1', '2021-08-26 00:00:00', '1');
+INSERT INTO `db_academico`.`malla_academica` (`maca_id`, `maca_tipo`, `maca_codigo`, `maca_nombre`, `maca_usuario_ingreso`, `maca_estado`, `maca_fecha_creacion`, `maca_estado_logico`) VALUES ('94', '2', 'GRA-0094', 'Psicología', '1', '1', '2021-08-26 00:00:00', '1');
+INSERT INTO `db_academico`.`malla_academica` (`maca_id`, `maca_tipo`, `maca_codigo`, `maca_nombre`, `maca_usuario_ingreso`, `maca_estado`, `maca_fecha_creacion`, `maca_estado_logico`) VALUES ('95', '2', 'GRA-0095', 'Ingeniería Ambiental', '1', '1', '2021-08-26 00:00:00', '1');
+INSERT INTO `db_academico`.`malla_academica` (`maca_id`, `maca_tipo`, `maca_codigo`, `maca_nombre`, `maca_usuario_ingreso`, `maca_estado`, `maca_fecha_creacion`, `maca_estado_logico`) VALUES ('96', '2', 'GRA-0096', 'Ingeniería Industrial', '1', '1', '2021-08-26 00:00:00', '1');

54bf57997d70e88f65740f69f30ada2da00a690d
root <root@localhost.localdomain>
2021-09-10 11:00:19 -0500

    DistributivoAca

--- modules/academico/views/matriculacion/newfund-search.php
+++ modules/academico/views/matriculacion/newfund-search.php
@@ -1,8 +1,7 @@
 <?php
 
-use yii\helpers\Html;
-use kartik\date\DatePicker;
 use app\modules\academico\Module as academico;
+use yii\helpers\Html;
 academico::registerTranslations();
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -10,30 +9,33 @@ academico::registerTranslations();
  * and open the template in the editor.
  */
 ?>
+
+
 <div class="row">
+
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <div class="form-group">                                  
-            <label for="cmb_asignaturaespos" class="col-sm-2 col-sm-2 col-lg-2 col-md-2 col-xs-2 control-label"><?= academico::t("matriculacion", "Academic Period") ?></label>
-            <div class="col-sm-3 col-xs-3 col-md-3 col-lg-3">
-                <?= Html::dropDownList("cmb_asignaturaespos", 0, $arr_periodo, ["class" => "form-control", "id" => "cmb_asignaturaespos"]) ?>
-            </div>   
-        </div>
-    </div>
-     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">  
         <div class="form-group">
-            <label for="txt_buscarDataCreate" class="col-sm-2 col-md-2 col-xs-2 col-lg-2 control-label"><?= Yii::t("formulario", "Search") ?></label>
-            <div class="col-sm-8 col-md-8 col-xs-8 col-lg-8">
-                <input type="text" class="form-control" value="" id="txt_buscarDataCreate" placeholder="<?= Yii::t("solicitud_ins", "Search by SSN/Passport or Names") ?>">
+            <label for="cmb_asignaturaespos" class="col-sm-2 col-sm-2 col-lg-2 col-md-2 col-xs-2 control-label"><?=academico::t("matriculacion", "Academic Period")?></label>
+            <div class="col-sm-3 col-xs-3 col-md-3 col-lg-3">
+                <?=Html::dropDownList("cmb_periodo", 0, $arr_periodo, ["class" => "form-control", "id" => "cmb_periodo"])?>
             </div>
         </div>
     </div>
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">  
-        <div class="col-sm-8"></div>
-        <div class="col-sm-2">                
-            <a id="btn_buscarEstudiantes" href="javascript:" class="btn btn-primary btn-block"> <?= Yii::t("formulario", "Search") ?></a>
+     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="form-group">
+            <label for="txt_buscarDataCreate" class="col-sm-2 col-md-2 col-xs-2 col-lg-2 control-label"><?=Yii::t("formulario", "Search")?></label>
+            <div class="col-sm-8 col-md-8 col-xs-8 col-lg-8">
+                <input type="text" class="form-control" value="" id="txt_buscarDataCreate" placeholder="<?=Yii::t("solicitud_ins", "Search by SSN/Passport or Names")?>">
+            </div>
         </div>
     </div>
-    
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="col-sm-8"></div>
+        <div class="col-sm-2">
+            <a id="btn_buscarEstudiantes" href="javascript:" class="btn btn-primary btn-block"> <?=Yii::t("formulario", "Search")?></a>
+        </div>
+    </div>
+
 
 
 
--- modules/academico/views/matriculacion/newfund.php
+++ modules/academico/views/matriculacion/newfund.php
@@ -1,24 +1,36 @@
 <?php
 
 use yii\helpers\Html;
-use app\modules\academico\Module as academico;
 ?>
-<?= Html::hiddenInput('txth_ids', '', ['id' => 'txth_ids']); ?>
+<?=Html::hiddenInput('txth_ids', '', ['id' => 'txth_ids']);?>
+
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h3><span id="lbl_planear"> <i class='fas fa-user-tie'></i> <?=academico::t("matriculacion", "Registro Online - Administrativo")?></span></h3>
+        </div><br><br>
+
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h4><span id="lbl_planear"><i class="far fa-address-book"></i> <?=academico::t("matriculacion", "Listado de Estudiante a matricular")?></span></h4>
+        </div><br><br>
+    </div>
+
 <div>
     <form class="form-horizontal">
         <?=
-        $this->render('newfund-search', [ 
-            'arr_periodo' => $mod_periodo,
-    
-            ]);
-        ?>
+$this->render('newfund-search', [
+	'arr_periodo' => $mod_periodo,
+
+]);
+?>
     </form>
 </div>
 
 <div>
     <?=
-    $this->render('newfund-grid', [
-        'model' => $model,
-        ]);
-    ?>
+$this->render('newfund-grid', [
+	'model' => $model,
+]);
+?>
 </div>

b8557c1bd30da87bf207b75ae6800bf76c055b8f
GianVergara <analistadesarrollo02@uteg.edu.ec>
2021-11-15 11:47:52 -0500

    cambios en script de carreras grado

--- db/base_nueva_prod/data/script db_academico/actualizar_carreras15112021/actualizar_carreras_data15112021.sql
+++ db/base_nueva_prod/data/script db_academico/actualizar_carreras15112021/actualizar_carreras_data15112021.sql
@@ -1,15 +1,18 @@
 use db_academico;
 
-/* verificar id en produccion antes de correrlos */
+/* verificar id en producción antes de correrlos */
+-- Tecnologías de la Información
 UPDATE `db_academico`.`estudio_academico`
 SET `eaca_estado`='0', `eaca_estado_logico`='0'
 WHERE `eaca_id`='62';
 
+-- se llamaba anteriomente Educación Básica, se cambia nombre a Licenciatura en Educación Básica
 UPDATE `db_academico`.`estudio_academico`
-SET `eaca_nombre`='Licenciatura en Educación Báscica',
-`eaca_descripcion`='Licenciatura en Educación Báscica'
+SET `eaca_nombre`='Licenciatura en Educación Básica',
+`eaca_descripcion`='Licenciatura en Educación Básica'
 WHERE `eaca_id`='48';
 
+-- Educación
 UPDATE `db_academico`.`estudio_academico`
 SET `eaca_estado`='0', `eaca_estado_logico`='0'
 WHERE `eaca_id`='61';

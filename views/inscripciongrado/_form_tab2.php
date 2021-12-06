5cf3169f78d963e63f62b4a3a51145947fbb2fa5
GianVergara <analistadesarrollo02@uteg.edu.ec>
2021-12-06 17:17:53 -0500

    cambios varios

--- views/inscripciongrado/_form_tab2.php
+++ views/inscripciongrado/_form_tab2.php
@@ -397,9 +397,8 @@ $leyenda = '<div class="col-md-12 col-xs-12 col-sm-12 col-lg-12">
     </div>
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="form-group">
-            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2" style="text-align: right;">
-            </div>
-            <?= Html::a(Yii::t("formulario", "Leer términos y condiciones de adquisición de servicio educativo"), Url::to(['inscripciongrado/terminogrado', 'popup' => "true"]), ["class" => "pbpopup", "data-toggle" => "tooltip", "title" => "Términos", "data-pjax" => 0]); ?>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2" style="text-align: right;"></div>
+            <label for="terminos" class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><?= Html::a(Yii::t("formulario", "Leer términos y condiciones de adquisición de servicio educativo"), Url::to(['inscripciongrado/terminogrado', 'popup' => "true"]), ["class" => "pbpopup", "data-toggle" => "tooltip", "title" => "Términos", "data-pjax" => 0]); ?> </label>
         </div>
     </div>
     <div class="col-md-12 col-xs-12 col-sm-12 col-lg-12">

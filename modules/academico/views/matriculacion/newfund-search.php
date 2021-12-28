834a27acc3ad8e4662bdddcb7a3584049ae8564d
root <root@localhost.localdomain>
2021-12-28 11:33:58 -0500

    Cambios varios matriculación

--- modules/academico/views/matriculacion/newfund-search.php
+++ modules/academico/views/matriculacion/newfund-search.php
@@ -1,8 +1,7 @@
 <?php
 
-use yii\helpers\Html;
-use kartik\date\DatePicker;
 use app\modules\academico\Module as academico;
+use yii\helpers\Html;
 academico::registerTranslations();
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,36 +14,34 @@ academico::registerTranslations();
     <h3><?=Academico::t("matriculacion", "Registro en Línea - Administrativo")?></h3>
     </br>
     <h4><?=Academico::t("matriculacion", "Listado de estudiante a matricular")?></h4>
-    </br>    
+    </br>
 </div>
 
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
-            <div class="form-group">                                  
-                <label for="cmb_planificacion" class="col-sm-2 col-sm-2 col-lg-2 col-md-2 col-xs-2 control-label"><?= academico::t("matriculacion", "Academic Period") ?></label>
-                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                    <?= Html::dropDownList("cmb_planificacion", 0, $arr_pla, ["class" => "form-control", "id" => "cmb_planificacion"]) ?>
-                </div>   
-            </div>
-        </div>
-        
-    </div>
-     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">  
+    <br><br>
+     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="form-group">
-            <label for="txt_buscarDataCreate" class="col-sm-2 col-md-2 col-xs-2 col-lg-2 control-label"><?= Yii::t("formulario", "Search") ?></label>
+            <label for="txt_buscarDataCreate" class="col-sm-2 col-md-2 col-xs-2 col-lg-2 control-label"><?=Yii::t("formulario", "Search")?></label>
             <div class="col-sm-8 col-md-8 col-xs-8 col-lg-8">
-                <input type="text" class="form-control" value="" id="txt_buscarDataCreate" placeholder="<?= Yii::t("solicitud_ins", "Search by SSN/Passport or Names") ?>">
+                <input type="text" class="form-control" value="" id="txt_buscarDataCreate" placeholder="<?=Yii::t("solicitud_ins", "Search by SSN/Passport or Names")?>">
             </div>
         </div>
     </div>
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">  
-        <div class="col-sm-8"></div>
-        <div class="col-sm-2">                
-            <a id="btn_buscarEstudiantes" href="javascript:" class="btn btn-primary btn-block"> <?= Yii::t("formulario", "Search") ?></a>
+    <div class='col-xs-12 col-sm-12 col-md-6 col-lg-6'>
+        <div class="form-group">
+            <label for="cmb_planificacion" class="col-xs-12 col-sm-12 col-md-5 col-lg-5 control-label"><?=academico::t("matriculacion", "Academic Period")?></label>
+            <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+                    <?=Html::dropDownList("cmb_planificacion", 0, $arr_pla, ["class" => "form-control", "id" => "cmb_planificacion"])?>
+            </div>
         </div>
     </div>
-    
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="col-sm-8"></div>
+        <div class="col-sm-2">
+            <a id="btn_buscarEstudiantes" href="javascript:" class="btn btn-primary btn-block"> <?=Yii::t("formulario", "Search")?></a>
+        </div>
+    </div>
+
 
 
 

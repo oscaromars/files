882ea92c6880d037366ab16cef80995c89172092
GianVergara <analistadesarrollo02@uteg.edu.ec>
2022-01-07 11:20:46 -0500

    cambios en modificar estudio académico

--- modules/academico/views/estudioacademico/js/estudioacademico.js
+++ modules/academico/views/estudioacademico/js/estudioacademico.js
@@ -40,27 +40,25 @@ function savestudioacademico() {
     }
 }
 
-/*function updatedistributivohorario() {
-    var link = $('#txth_base').val() + "/academico/distributivoacademicohorario/updatedistributivohorario";
+function updatestudioacademico() {
+    var link = $('#txth_base').val() + "/academico/estudioacademico/updatestudioacademico";
     var arrParams = new Object();
-    arrParams.id = $("#frm_daho_id").val();
-    arrParams.modalidad = $('#distributivoacademicohorario-mod_id').val();
-    arrParams.estudio = $('#distributivoacademicohorario-eaca_id').val();
-    arrParams.unidad = $('#distributivoacademicohorario-uaca_id').val();
-    arrParams.descripcion = $('#distributivoacademicohorario-daho_descripcion').val();
-    arrParams.jornada = $('#distributivoacademicohorario-daho_jornada').val();
-    arrParams.estado = $('#distributivoacademicohorario-daho_estado').val();
-    arrParams.horario = $('#distributivoacademicohorario-daho_horario').val();
-    arrParams.totalhora = $('#distributivoacademicohorario-daho_total_horas').val();
+    arrParams.id = $("#frm_eaca_id").val();
+    arrParams.teac_id = $('#estudioacademico-teac_id').val();
+    arrParams.eaca_nombre = $('#estudioacademico-eaca_nombre').val();
+    arrParams.eaca_descripcion = $('#estudioacademico-eaca_descripcion').val();
+    arrParams.eaca_alias_resumen = $('#estudioacademico-eaca_alias_resumen').val();
+    arrParams.eaca_alias = $('#estudioacademico-eaca_alias').val();
+    arrParams.estado = $('#estudioacademico-eaca_estado').val();
 
     if (!validateForm()) {
         requestHttpAjax(link, arrParams, function(response) {
             showAlert(response.status, response.label, response.message);
             if (response.status == "OK") {
                 setTimeout(function() {
-                    window.location.href = $('#txth_base').val() + "/academico/distributivoacademicohorario/index";
+                    window.location.href = $('#txth_base').val() + "/academico/estudioacademico/index";
                     }, 3000);
             }
         }, true);
     }
-}*/
+}

23ad191cbd952e2b3f6e56e5cf440b51be030b83
analistadesarrollo05 <analistadesarrollo05@uteg.edu.ec>
2022-01-26 19:33:29 -0500

    actualizacion Planificacion Pes

--- modules/academico/controllers/PlanificacionController.php
+++ modules/academico/controllers/PlanificacionController.php
@@ -177,7 +177,6 @@ concat(per.per_pri_nombre, ' ', ifnull(per.per_seg_nombre,''), ' ', per.per_pri_
      where  malle.maca_id = maca.maca_id  AND
          meu.mod_id = :modalidad and meu.uaca_id = 1 and mpmo.saca_id = :periodo and mpmo.mpmo_activo = 'A'
           and mpmo.mpmo_procesado not in (0,1) 
-    and mpmo.mpmo_activo = Null
     AND  mpmo.mpmo_estado = 1 AND mpmo.mpmo_estado_logico = 1
     AND  e.est_estado = 1 AND e.est_estado_logico = 1
     AND  c.ecpr_estado = 1 AND c.ecpr_estado_logico = 1

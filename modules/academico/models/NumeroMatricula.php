f4a54480b7306b347c5ca9aa85d3c2874fc66a18
GianVergara <analistadesarrollo02@uteg.edu.ec>
2022-02-03 12:07:12 -0500

    Crear script de consultas para numero matricula

--- modules/academico/models/NumeroMatricula.php
+++ modules/academico/models/NumeroMatricula.php
@@ -72,4 +72,129 @@ class NumeroMatricula extends \yii\db\ActiveRecord
             'nmat_estado_logico' => 'Nmat Estado Logico',
         ];
     }
+
+    /**
+     * Function consultaNumatriculacod
+     * @author  Giovanni Vergara <analistadesarrollo02@uteg.edu.ec>
+     * @property integer
+     * @return
+     */
+    public function consultaNumatriculacod($nmat_codigo) {
+        $con = \Yii::$app->db_academico;
+        $numero = 0;
+        $estado = 1;
+                $sql = "SELECT nmat_id,
+                            IFNULL(CAST(nmat_numero AS UNSIGNED),0) secuencia
+                        FROM " . $con->dbname . ".numero_matricula
+                        WHERE nmat_estado = :estado AND
+                            nmat_estado_logico= :estado AND
+                            nmat_codigo = :nmat_codigo";
+                $sql .= "  ";
+                $comando = $con->createCommand($sql);
+                $comando->bindParam(":estado", $estado, \PDO::PARAM_STR);
+                $comando->bindParam(":nmat_codigo", $nmat_codigo, \PDO::PARAM_INT);
+                $resultData = $comando->queryOne();
+                return $resultData;
+    }
+    /**
+     * Function modifica numero_matricula.
+     * @author Giovanni Vergara <analistadesarrollo02@uteg.edu.ec>;
+     * @param
+     * @return
+     */
+    public function actualizarSecmatricula($nmat_id, $nmat_numero) {
+
+        $con = \Yii::$app->db_academico;
+        $nmat_fecha_modificacion = date(Yii::$app->params["dateTimeByDefault"]);
+        $nmat_usuario_modifica = @Yii::$app->session->get("PB_iduser");
+        if ($trans !== null) {
+            $trans = null; // si existe la transacción entonces no se crea una
+        } else {
+            $trans = $con->beginTransaction(); // si no existe la transacción entonces se crea una
+        }
+        try {
+            $comando = $con->createCommand
+                    ("UPDATE " . $con->dbname . ".numero_matricula
+                      SET nmat_numero = :nmat_numero,
+                          nmat_usuario_modifica = :nmat_usuario_modifica,
+                          nmat_fecha_modificacion = :nmat_fecha_modificacion
+                      WHERE
+                      nmat_id = :nmat_id ");
+            $comando->bindParam(":nmat_id", $nmat_id, \PDO::PARAM_INT);
+            $comando->bindParam(":nmat_usuario_modifica", $nmat_usuario_modifica, \PDO::PARAM_INT);
+            $comando->bindParam(":nmat_fecha_modificacion", $nmat_fecha_modificacion, \PDO::PARAM_STR);
+            $comando->bindParam(":nmat_numero", $nmat_numero, \PDO::PARAM_STR);
+            $response = $comando->execute();
+            if ($trans !== null)
+                $trans->commit();
+            return $response;
+        } catch (Exception $ex) {
+            if ($trans !== null)
+                $trans->rollback();
+            return FALSE;
+        }
+    }
+
+     /**
+     * Function consultaNumatricula año y secuencial
+     * @author
+     * @property integer
+     * @return
+     */
+    public function consultaNumatricula() {
+        $con = \Yii::$app->db_academico;
+        $numero = 0;
+        $estado = 1;
+                $sql = "SELECT nmat_id, nmat_anio
+                            IFNULL(CAST(nmat_numero AS UNSIGNED),0) secuencia
+                        FROM " . $con->dbname . ".numero_matricula
+                        WHERE nmat_estado = :estado AND
+                            nmat_estado_logico= :estado AND
+                            nmat_anio = :nmat_anio";
+                $sql .= "  ";
+                $comando = $con->createCommand($sql);
+                $comando->bindParam(":estado", $estado, \PDO::PARAM_STR);
+                $comando->bindParam(":nmat_anio", $nmat_anio, \PDO::PARAM_STR);
+                $resultData = $comando->queryOne();
+                return $resultData;
+    }
+
+    /**
+     * Function modifica actualizarAniomatricula.
+     * @author Giovanni Vergara <analistadesarrollo02@uteg.edu.ec>;
+     * @param
+     * @return
+     */
+    public function actualizarAniomatricula($nmat_id, $nmat_anio) {
+
+        $con = \Yii::$app->db_academico;
+        $nmat_fecha_modificacion = date(Yii::$app->params["dateTimeByDefault"]);
+        $nmat_usuario_modifica = @Yii::$app->session->get("PB_iduser");
+        if ($trans !== null) {
+            $trans = null; // si existe la transacción entonces no se crea una
+        } else {
+            $trans = $con->beginTransaction(); // si no existe la transacción entonces se crea una
+        }
+        try {
+            $comando = $con->createCommand
+                    ("UPDATE " . $con->dbname . ".numero_matricula
+                      SET nmat_anio = :nmat_anio,
+                          nmat_usuario_modifica = :nmat_usuario_modifica,
+                          nmat_fecha_modificacion = :nmat_fecha_modificacion
+                      WHERE
+                      nmat_id = :nmat_id ");
+            $comando->bindParam(":nmat_id", $nmat_id, \PDO::PARAM_INT);
+            $comando->bindParam(":nmat_usuario_modifica", $nmat_usuario_modifica, \PDO::PARAM_INT);
+            $comando->bindParam(":nmat_fecha_modificacion", $nmat_fecha_modificacion, \PDO::PARAM_STR);
+            $comando->bindParam(":nmat_anio", $nmat_anio, \PDO::PARAM_STR);
+            $response = $comando->execute();
+            if ($trans !== null)
+                $trans->commit();
+            return $response;
+        } catch (Exception $ex) {
+            if ($trans !== null)
+                $trans->rollback();
+            return FALSE;
+        }
+    }
 }

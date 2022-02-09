d19651bf9a13445e2ffe38652bbd39bac932abd1
GianVergara <analistadesarrollo02@uteg.edu.ec>
2022-02-09 11:12:18 -0500

    ya no se anula solicitud de inscripción, solo se modifica y cuando el estado de la solicitud sea pendiente

--- modules/admision/views/solicitudes/_listarSolxinteresadoGrid.php
+++ modules/admision/views/solicitudes/_listarSolxinteresadoGrid.php
@@ -73,21 +73,21 @@ PbGridView::widget([
             'template' => '{view} {cancel} {payments} {upload}', //
             'buttons' => [
                 'view' => function ($url, $model) {
-                    $mod_ordpago = new OrdenPago;
-                    $result = $mod_ordpago->consultarImagenpagoexiste($model['opag_id']);
-                    if ($result['existe_imagen'] == 0 && $model['estado_pago'] != 'Pagado') { // Aqui si la solicitud esta pendiente  y no ha subido pago
+                    //$mod_ordpago = new OrdenPago;
+                    //$result = $mod_ordpago->consultarImagenpagoexiste($model['opag_id']);
+                    if (/*$result['existe_imagen'] == 0 && */ $model['estado_pago'] != 'Pagado') { // Aqui si la solicitud esta pendiente de aprobar se puede modificar
                         return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', Url::to(['/admision/solicitudes/viewsolicitud', 'id_sol' => base64_encode($model['sins_id']), 'per_id' => base64_encode($model['per_id']), 'opag_id' => base64_encode($model['opag_id'])]), ["data-toggle" => "tooltip", "title" => "Ver Solicitud", "data-pjax" => 0]);
                     } else {
                         return '<span class="glyphicon glyphicon-eye-open"></span>';
                     }
                 },
-                'cancel' => function ($url, $model) {
+                /*'cancel' => function ($url, $model) {
                     if ($model['estado'] == 'Pendiente' && $model['estado_pago'] == 'Pagado') { // Aqui si la solicitud esta pendiente  y no ha subido pago
                         return Html::a('<span class="glyphicon glyphicon-trash"></span>', "#", ['onclick' => "anularsolicitud(" . $model['sins_id'] . ",". $model['opag_id'] .",". $model['per_id'] .");", "data-toggle" => "tooltip", "title" => "Anular Solicitud", "data-pjax" => 0]);
                     } else {
                         return '<span class="glyphicon glyphicon-trash"></span>';
                     }
-                },
+                },*/
                 'payments' => function ($url, $model) {
                     return Html::a('<span class="glyphicon glyphicon-usd"></span>', Url::to(['/financiero/pagos/listarpagosolicitud', 'id_sol' => base64_encode($model['sins_id']), 'per_id' => $_GET['perid']]), ["data-toggle" => "tooltip", "title" => "Pago de Solicitud", "data-pjax" => 0]);
                 },

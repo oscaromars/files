29c61db737c6444d77e38be071602552bfa7b30d
Grace Viteri <analistadesarrollo01@uteg.edu.ec>
2022-03-01 22:21:24 -0500

    Modelo Persona

--- db/base_nueva_prod/data/Script_Instituto de Ciencias Políticas.sql
+++ db/base_nueva_prod/data/Script_Instituto de Ciencias Políticas.sql
@@ -5,7 +5,7 @@ VALUES ('10', 'Instituto de Ciencias Políticas', 'Instituto de Ciencias Políti
 
 /** Verificar PK produccion*/
 INSERT INTO `db_academico`.`estudio_academico` (`eaca_id`, `teac_id`, `eaca_codigo`, `eaca_nombre`, `eaca_descripcion`, `eaca_alias`, `eaca_alias_resumen`, `eaca_usuario_ingreso`, `eaca_usuario_modifica`, `eaca_estado`, `eaca_fecha_creacion`, `eaca_estado_logico`)
-VALUES ('77', '3', 'DFPP', 'Diplomado en Formación y participación política', 'Diplomado en Formación y participación política', 'diplomado_institucion', 'diplomado_institucion', '1', NULL, '1', '2022-02-25 11:31:00', '1');
+VALUES ('77', '3', 'DFPP', 'Diplomado en Formación y Participación Política', 'Diplomado en Formación y Participación Política', 'diplomado_institucion', 'diplomado_institucion', '1', NULL, '1', '2022-02-25 11:31:00', '1');
 
 /*Verificar PK en produccion tanto el PK como el eaca_id que va en la columna 4*/
 INSERT INTO `db_academico`.`modalidad_estudio_unidad` (`meun_id`, `uaca_id`, `mod_id`, `eaca_id`, `emp_id`, `meun_usuario_ingreso`, `meun_estado`, `meun_fecha_creacion`, `meun_estado_logico`)

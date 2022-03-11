cef34e7672fc0fe4ecd5c8bdb6643e6a2856146a
analistadesarrollo05 <analistadesarrollo05@uteg.edu.ec>
2022-03-11 11:53:23 -0500

    Actualizacion solicitudes: Obtener detalles de convenios que afecten a items de matriculacion

--- modules/admision/models/ConvenioEmpresa.php
+++ modules/admision/models/ConvenioEmpresa.php
@@ -96,4 +96,41 @@ class ConvenioEmpresa extends \yii\db\ActiveRecord
         $resultData = $comando->queryAll();
         return $resultData;
     }
+
+        /**
+     * Function obtener detalles de convenios que afecten a items de matriculacion
+     * @author  Oscar Sanchez <analistadesarrollo05@uteg.edu.ec>;
+     * @property       
+     * @return  
+     */
+    public function consultarConvenioxMatricula($nint_id,$mod_id,$convenio,$ite_id) {
+        $con = \Yii::$app->db_captacion;
+        $con2 = \Yii::$app->db_facturacion;
+        $estado = 1;
+        $estado_convenio="A";
+            $sql = "SELECT cede.cede_id, cede.cemp_id, imun.ite_id, cede.uaca_id, cede.mod_id,
+        cede.cede_tipo, cede.cede_porcentaje_descuento, cede.cede_porcentaje_factor,
+        cemp.cemp_estado_empresa, cemp.cemp_fecha_inicio, cemp.cemp_fecha_fin
+        FROM " . $con->dbname . ".convenio_empresa_detalle as cede
+        INNER JOIN " . $con->dbname . ".convenio_empresa as cemp ON cemp.cemp_id = cede.cemp_id
+        INNER JOIN " . $con2->dbname . ".item_matricula_unidad  as imun ON imun.uaca_id = cede.uaca_id AND  imun.mod_id = cede.mod_id
+              WHERE TRUE
+                AND cemp.cemp_estado = :estado AND cemp.cemp_estado_logico = :estado 
+                AND cede.cede_estado = :estado AND cede.cede_estado_logico = :estado 
+                AND imun.imun_estado = :estado AND imun.imun_estado_logico = :estado 
+                AND cede.uaca_id = :nivel AND cede.mod_id = :modalidad
+                AND cede.cede_tipo = 'MA' AND cede.cemp_id = :convenio
+                AND imun.ite_id = :item AND cemp.cemp_estado_empresa = :estado_convenio
+                AND cemp.cemp_fecha_inicio <= NOW()
+                AND cemp.cemp_fecha_fin > NOW()";
+        $comando = $con->createCommand($sql);
+        $comando->bindParam(":nivel", $nint_id, \PDO::PARAM_INT);  
+        $comando->bindParam(":modalidad", $mod_id, \PDO::PARAM_INT);  
+        $comando->bindParam(":convenio", $convenio, \PDO::PARAM_INT);
+        $comando->bindParam(":item", $ite_id, \PDO::PARAM_INT);
+        $comando->bindParam(":estado", $estado, \PDO::PARAM_STR);        
+        $comando->bindParam(":estado_convenio", $estado_convenio, \PDO::PARAM_STR); 
+        $resultData = $comando->queryAll();
+        return $resultData;
+    }
 }

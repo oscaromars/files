24598515030c1928ac397b867a043c3ff40c391b
GianVergara <analistadesarrollo02@uteg.edu.ec>
2022-04-25 14:36:01 -0500

    filtros por unidad academica vista, excel y pdf en aspirantes

--- modules/admision/views/interesados/index-search.php
+++ modules/admision/views/interesados/index-search.php
@@ -5,7 +5,7 @@ use kartik\date\DatePicker;
 ?>
 
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">  
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="form-group">
             <label for="txt_buscarData" class="col-sm-2 col-md-2 col-xs-2 col-lg-2 control-label"><?= Yii::t("formulario", "Search") ?></label>
             <div class="col-sm-8 col-md-8 col-xs-8 col-lg-8">
@@ -47,21 +47,21 @@ use kartik\date\DatePicker;
             </div>
         </div>
     </div>
-    <div class='col-md-12 col-sm-12 col-xs-12 col-lg-12'>                   
+    <div class='col-md-12 col-sm-12 col-xs-12 col-lg-12'>
         <div class="form-group">
-            <label for="cmb_empresa" class="col-sm-2 col-lg-2 col-md-2 col-xs-2 control-label"><?= Yii::t("formulario", "Company") ?>  </label>
-            <div class="col-sm-3 col-md-3 col-xs-3 col-lg-3"> 
-                <?= Html::dropDownList("cmb_empresa", 0, array_merge([Yii::t("formulario", "All")], $arr_empresa), ["class" => "form-control", "id" => "cmb_empresa"]) ?> 
+            <label for="cmb_unidad" class="col-sm-2 col-lg-2 col-md-2 col-xs-2 control-label"><?= Yii::t("formulario", "Academic unit") ?>  </label>
+            <div class="col-sm-3 col-md-3 col-xs-3 col-lg-3">
+            <?= Html::dropDownList("cmb_unidad", 0, $arr_unidad, ["class" => "form-control", "id" => "cmb_unidad"]) ?>
             </div>
-            <!-- <label for="cmb_unidad" class="col-sm-2 col-lg-2 col-md-2 col-xs-2 control-label"><? Yii::t("formulario", "Academic unit") ?>  </label>
-            <div class="col-sm-3 col-md-3 col-xs-3 col-lg-3"> 
-                <? Html::dropDownList("cmb_unidad", 0, $arr_unidad, ["class" => "form-control", "id" => "cmb_unidad"]) ?> 
-            </div>-->
-        </div>              
+            <label for="cmb_empresa" class="col-sm-2 col-lg-2 col-md-2 col-xs-2 control-label"><?= Yii::t("formulario", "Company") ?>  </label>
+            <div class="col-sm-3 col-md-3 col-xs-3 col-lg-3">
+                <?= Html::dropDownList("cmb_empresa", 0, array_merge([Yii::t("formulario", "All")], $arr_empresa), ["class" => "form-control", "id" => "cmb_empresa"]) ?>
+            </div>
+        </div>
     </div>
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">  
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="col-sm-8"></div>
-        <div class="col-sm-2">                
+        <div class="col-sm-2">
             <a id="btn_buscarInteresado" href="javascript:" class="btn btn-primary btn-block"> <?= Yii::t("formulario", "Search") ?></a>
         </div>
     </div>
--- modules/admision/views/interesados/js/interesados.js
+++ modules/admision/views/interesados/js/interesados.js
@@ -9,10 +9,10 @@ function actualizarGridInteresado(){
     var f_ini = $('#txt_fecha_ini').val();
     var f_fin = $('#txt_fecha_fin').val();
     var empresa = $('#cmb_empresa option:selected').val();
-    // var unidad = $('#cmb_unidad option:selected').val();
+    var unidad = $('#cmb_unidad option:selected').val();
     if (!$(".blockUI").length) {
         showLoadingPopup();
-        $('#TbG_Interesado').PbGridView('applyFilterData', {'search': interesado, 'f_ini': f_ini, 'f_fin': f_fin, 'company': empresa/*, 'unidad': unidad*/});
+        $('#TbG_Interesado').PbGridView('applyFilterData', {'search': interesado, 'f_ini': f_ini, 'f_fin': f_fin, 'company': empresa, 'unidad': unidad});
         setTimeout(hideLoadingPopup, 2000);
     }
 }
@@ -21,17 +21,17 @@ function exportExcel() {
     var search = $('#txt_buscarData').val();
     var f_ini = $('#txt_fecha_ini').val();
     var f_fin = $('#txt_fecha_fin').val();
-    var empresa = $('#cmb_empresa option:selected').val(); 
-    // var unidad = $('#cmb_unidad option:selected').val();
-    window.location.href = $('#txth_base').val() + "/admision/interesados/expexcel?search=" + search + "&f_ini=" + f_ini + "&f_fin=" + f_fin + "&empresa=" + empresa ;
-   
+    var empresa = $('#cmb_empresa option:selected').val();
+    var unidad = $('#cmb_unidad option:selected').val();
+    window.location.href = $('#txth_base').val() + "/admision/interesados/expexcel?search=" + search + "&f_ini=" + f_ini + "&f_fin=" + f_fin + "&empresa=" + empresa + "&unidad=" + unidad;
+
 }
 
 function exportPdfAspirante(){
     var search = $('#txt_buscarData').val();
     var f_ini = $('#txt_fecha_ini').val();
     var f_fin = $('#txt_fecha_fin').val();
-    var empresa = $('#cmb_empresa option:selected').val();  
-    // var unidad = $('#cmb_unidad option:selected').val();
-    window.location.href = $('#txth_base').val() + "/admision/interesados/exppdfaspirantes?pdf=1&search=" + search + "&f_ini=" + f_ini + "&f_fin=" + f_fin + "&empresa=" + empresa;   
+    var empresa = $('#cmb_empresa option:selected').val();
+    var unidad = $('#cmb_unidad option:selected').val();
+    window.location.href = $('#txth_base').val() + "/admision/interesados/exppdfaspirantes?pdf=1&search=" + search + "&f_ini=" + f_ini + "&f_fin=" + f_fin + "&empresa=" + empresa+ "&unidad=" + unidad;
 }
